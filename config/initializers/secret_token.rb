2302d2d022ee1244cfc7551e618444bb4f30ceb9
Jeff Conner <drumwagon@gmail.com>
2013-12-07 11:20:34 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crawl::Application.config.secret_key_base = 'f2cc3bff9a8af0fc73bfd03e21adfbfb9dddaa76caa0126c7cd9fcb56e7ae47d690578e8f3ad53a2f7cd505d2ab898f1bcc2e902e1c073d14edba9e6df024192'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crawl::Application.config.session_store :cookie_store, key: '_crawl_session'
