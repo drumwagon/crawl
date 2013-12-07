1371101e56b9bec89ede4fb4e16e630b73816c04
Jeff Conner <drumwagon@gmail.com>
2013-12-07 11:58:12 -0600

    Add routes and stops

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+Route.destroy_all
+puts 'Wiped out all routes'
+
+routes = [
+		{
+			name: "north-south"
+		},
+		{
+			name: "south-north"
+		},
+		{
+			name: "random"
+		}
+	]
+
+routes.each do |routes|
+	Route.create(routes)
+end
+puts "Created 4 routes"
+
+Stop.destroy_all
+puts 'Wiped out all stops'
+
+stops = [
+		{
+			name: "Burnett"
+		},
+		{
+			name: "East 6th"
+		}, 
+		{
+			name: "Rainey"
+		},
+		{
+			name: "SoCo"
+		}
+	]
+]
