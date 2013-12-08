4bcef839c81f6e045b2d891c6698a1d50721bb8e
Jeff Conner <drumwagon@gmail.com>
2013-12-08 16:46:35 -0600

    fix linking issues

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
   def index
+  	@stop_spot = Stop.all
   end
 
   def show
--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,7 +1,10 @@
 class StopsController < ApplicationController
   def index
+  	@stop_spot = Stop.all
   end
   def show
   	@stop = Stop.find(params[:id])
   end
+  
+
 end
